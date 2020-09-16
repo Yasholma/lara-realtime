2822e6b93771ae8785c52efad7d393738c21ead0
Yahaya Hyellamoda Yohanna <yasholma1@gmail.com>
2020-09-16 09:27:07 +0100

    feat: install jwt package
    add routes and create auth controller

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,14 @@ Route::apiResource('/questions/{question}/replies', 'ReplyController');
 // Likes
 Route::post('/replies/{reply}/like', 'LikeController@like');
 Route::delete('/replies/{reply}/like', 'LikeController@unlike');
+
+// Authentication
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});

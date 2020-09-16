e068b65214796770efa996fdee34aab1b6232f12
Yahaya Hyellamoda Yohanna <yasholma1@gmail.com>
2020-09-16 16:45:25 +0100

    install vuetify, vue router and connect home blade file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+import Vuetify from "vuetify";
+import VueRouter from "vue-router";
+import router from "./routes/routes";
+
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+Vue.component("AppHome", require("./components/AppHome.vue").default);
+
+const app = new Vue({
+    el: "#app",
+    vuetify: new Vuetify(),
+    router
+});
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card color="grey lighten-4" flat height="200px" tile>
+    <v-toolbar dense>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title>CyberCode</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Forum</v-btn>
+        <v-btn text>Ask Question</v-btn>
+        <v-btn text>Category</v-btn>
+        <v-btn text>Login</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-card>
+</template>
